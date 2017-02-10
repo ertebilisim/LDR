7d64f67c7b5193687ddc5227652127df4c28d8af
ertebilisim <ertebilisim@gmail.com>
2017-02-10 13:12:11 +0200

    Add files via upload

---
+++ LDR.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+Servo servo;
+
+const byte buzzer = 8;
+const byte servoPin = 9;
+void setup() {
+  Serial.begin(9600);
+  servo.attach(servoPin);
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer, LOW);
+  servo.write(0);
+  delay(1000); 
+}
+int ldrValue;
+void loop() {
+  ldrValue = analogRead(A0);
+  Serial.println(ldrValue);
+  if (ldrValue < 1000)
+  {
+    digitalWrite(buzzer, HIGH);
+    servo.write(0);
+    
+  }
+  else
+  {
+    digitalWrite(buzzer, LOW);
+    servo.write(90);
+    delay(1000);
+   }
+}
